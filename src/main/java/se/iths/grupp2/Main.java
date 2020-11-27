eb7bbe48aab74fe2c92ac485e9d23594fb7fc539
marcuslark <marcus.lark.stahlberg@iths.se>
2020-11-27 11:23:37 +0100

    marcus branch tillagd

--- src/main/java/se/iths/grupp2/Main.java
+++ src/main/java/se/iths/grupp2/Main.java
@@ -4,6 +4,8 @@ import se.iths.grupp2.contact.ContactBook;
 
 public class Main {
 
+    //Marcus branch tillagd
+
     private static final ContactBook contactBook = new ContactBook();
 
     public static ContactBook getContactBook() {
