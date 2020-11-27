c3825479e689e22ac740413769c25a4a85bb9f48
Kabbej <kabbe.jartun@gmail.com>
2020-11-27 11:22:35 +0100

    first test commit

--- src/main/java/se/iths/grupp2/Main.java
+++ src/main/java/se/iths/grupp2/Main.java
@@ -4,6 +4,8 @@ import se.iths.grupp2.contact.ContactBook;
 
 public class Main {
 
+    //Testar push med endast textredigering
+
     private static final ContactBook contactBook = new ContactBook();
 
     public static ContactBook getContactBook() {
