6768d3966bbe5df45426783891edb7812d1cf167
Tobias <twadseth@icloud.com>
2020-11-27 10:51:10 +0100

    Removed exception handling

--- src/main/java/se/iths/grupp2/pages/Index.java
+++ src/main/java/se/iths/grupp2/pages/Index.java
@@ -6,15 +6,15 @@ import se.iths.grupp2.Main;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.io.IOException;
 
 @Path("hello")
 public class Index {
 
     @GET
-    public String display() throws IOException {
+    public String display() {
         ContactBook contactBook = Main.getContactBook();
         contactBook.addContact(new Contact("Test", "123456789"));
+        contactBook.addContact(new Contact("Marcus", "987654321"));
 
         StringBuilder format = new StringBuilder();
         for (Contact contact : contactBook.getContactList()) {
